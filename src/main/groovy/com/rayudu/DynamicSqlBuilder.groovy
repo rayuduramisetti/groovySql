4c57f070b7237ab1bdfada08f54aaf927aae526e
Rayudu <rayuduramisetti26@gmail.com>
2017-05-08 16:19:58 -0500

    Created Join, where, orderBy and pagination

--- src/main/groovy/com/rayudu/DynamicSqlBuilder.groovy
+++ src/main/groovy/com/rayudu/DynamicSqlBuilder.groovy
@@ -3,11 +3,15 @@ package com.rayudu
 /**
  * Created by Ramisetti on 5/3/17.
  */
-public class DynamicSqlBuilder extends FactoryBuilderSupport {
+class DynamicSqlBuilder extends FactoryBuilderSupport {
 
     {
         registerFactory('select', new SelectFactory())
         registerFactory('where', new WhereFactory())
+        registerFactory('orderBy', new SqlOrderByFactory())
+        registerFactory('pagination', new SqlPaginationFactory())
+        registerFactory('join', new JoinFactory())
+        registerFactory('on', new OnFactory())
     }
 }
 
@@ -15,20 +19,84 @@ class Select {
 
     String table
     List clauses = []
+    def orderBy
+    def pagination
+
+    List joins = []
+    List tables = []
+    List on = []
 
     String build() {
         String sql = "SELECT * FROM $table"
-        clauses.each { Where where ->
-            where.clauses.each {
-                sql += it
+        if (joins) {
+            joins.each { Join join ->
+                sql += " ${join.type ?: 'INNER'} JOIN ${join.table} ON"
+                join.on.eachWithIndex {On on, idx ->
+                    if (idx > 0) sql += " AND"
+                    sql += " ${on.left} ${on.operator ?: '='} ${on.right}"
+                }
             }
         }
+        clauses.eachWithIndex { Where where, int index ->
+            sql += "${ index == 0 ? ' WHERE' : ' OR' } ("
+            where.clauses.eachWithIndex { String clause, int indx ->
+                sql += "${ indx == 0 ? '' : ' AND' } $clause"
+            }
+           sql += ' )'
+        }
+        if (orderBy) {
+            if (orderBy.sort?.contains(',')) {
+                sql += " ORDER BY ${orderBy.sort.replaceAll('\\+', ' ')}\n"
+            } else if (orderBy.sort) {
+                sql += addOrderBy(orderBy)
+            } else if (orderBy.sorting) {
+                String sorting = orderBy.sorting.replaceAll('\\+', ' ')
+                sql += " ORDER BY $sorting\n"
+            }
+        }
+        if (pagination) {
+            sql += pagination.pageSize ? " OFFSET ${pagination.offset ?: 0} ROWS FETCH NEXT ${pagination.pageSize ?: 25} ROWS ONLY" : ''
+        }
         sql
+    }
+
+    private String addOrderBy(SqlOrderBy orderBy) {
+        orderBy.sort ? " ORDER BY $orderBy.sort ${orderBy.order?.toUpperCase() ?: 'ASC'}\n" : ''
     }
 }
 
 class Where {
     List<String> clauses = []
+}
+
+class SqlOrderBy {
+
+    String sort
+    String order
+    String sorting
+
+    String getOrder() {
+        order?.toUpperCase()
+    }
+}
+
+class SqlPagination {
+
+    Integer offset
+    Integer pageSize
+}
+
+class Join {
+
+    String table
+    String type = 'INNER'
+    List on = []
+}
+
+class On {
+    String left
+    String right
+    String operator
 }
 
 class SelectFactory extends AbstractFactory {
@@ -38,13 +106,21 @@ class SelectFactory extends AbstractFactory {
         new Select(table: value)
     }
 
-    public void setChild(FactoryBuilderSupport factoryBuilderSupport, Object parent, Object child) {
+    void setChild(FactoryBuilderSupport factoryBuilderSupport, Object parent, Object child) {
         if (child instanceof Where) {
             parent.clauses << child
         }
+        if (child instanceof SqlPagination) {
+            parent.pagination = child
+        }
+        if (child instanceof SqlOrderBy) {
+            parent.orderBy = child
+        }
+        if (child instanceof Join) {
+            parent.joins << child
+        }
     }
 }
-
 
 class WhereFactory extends AbstractFactory {
 
@@ -56,4 +132,57 @@ class WhereFactory extends AbstractFactory {
     boolean isLeaf() { true }
 }
 
+class SqlOrderByFactory extends AbstractFactory {
+
+    Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+        new SqlOrderBy(sort: attributes.sort, order: attributes.order, sorting: attributes.sorting)
+    }
+
+    boolean isLeaf() { true }
+}
+
+class SqlPaginationFactory extends AbstractFactory {
+
+    Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+        new SqlPagination(offset: attributes.offset, pageSize: attributes.pageSize)
+    }
+
+    boolean isLeaf() { true }
+}
+
+class JoinFactory extends AbstractFactory {
+
+    Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+        new Join(table: value, type: attributes.type)
+    }
+
+    void setChild(FactoryBuilderSupport factoryBuilderSupport, Object parent, Object child) {
+        super.setChild(factoryBuilderSupport, parent, child)
+        if (child instanceof On) {
+            parent.on << child
+        }
+    }
+
+    void setParent(FactoryBuilderSupport factoryBuilderSupport, Object parent, Object child) {
+        super.setParent(factoryBuilderSupport, parent, child)
+        parent.tables << child.table
+    }
+}
+
+class OnFactory extends AbstractFactory {
+
+    Object newInstance(FactoryBuilderSupport builder, Object name, Object value, Map attributes) {
+        On on = new On()
+        if (value) { on.operator = value }
+        attributes.each { k, v ->
+            if (!on.left) {
+                on.left = "$k.$v"
+            } else if (!on.right) {
+                on.right = "$k.$v"
+            }
+        }
+        attributes.clear()
+        on
+    }
+}
 
--- src/test/groovy/com/rayudu/GrvySqlGradleTest.groovy
+++ src/test/groovy/com/rayudu/GrvySqlGradleTest.groovy
@@ -6,14 +6,23 @@ class GrvySqlGradleTest {
 
     @Test
     void buildSelectSql() {
-      println  dSB().select('Example') {
-          where(['x = :y'])
+      println  dSB().select('User') {
+          join ('UserPermission', type: 'OUTER') {
+              on('User': 'userId', 'UserPermission': 'userId')
+              on('User': 'userName', 'UserPermission': 'userName')
+          }
+          where(['firstName = :firstName', 'lastName = :lastName'])
+          where(['userName = :userName'])
+          //orderBy(sorting: 'userName+DESC')
+          //orderBy(sort: 'userName,firstName')
+          orderBy(sort: 'userName+DESC,firstName+ASC')
+          //orderBy(sort: 'userName')
+          //orderBy(sort: 'userName', order: 'desc')
+          pagination(offset: 0, pageSize: 3)
       }.build()
-
     }
 
     private dSB() {
         new DynamicSqlBuilder()
     }
-
 }
